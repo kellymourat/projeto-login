04a4c6df958dd913fdf03c337321b215656a9978
kellymourat <kellymoura.pt@gmail.com>
2025-06-16 13:06:54 +0100

    Update form

---
+++ login.php
@@ -0,0 +1,5 @@
+Formulário enviado com sucesso! Seus dados foram armazenados com segurança em nosso banco de dados, com acesso restrito e criptografia. 
+
+Para saber mais, consulte nossa [Política de Privacidade].
+
+Obrigado por confiar em nós!
